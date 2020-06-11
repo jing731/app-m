09dd5b13e61167f40af599d66c8543ce5c046b8f
jing <3223191529@qq.com>
2020-06-11 17:12:59 +0800

    token本地持久化完成

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,14 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    user: null
+    user: window.localStorage.getItem('user')
   },
   mutations: {
     setUser (state, data) {
       state.user = data
+      // 为了防止页面刷新导致数据丢失，需要将数据放到本地存储，
+      // 这里的存储仅仅是为了持久化数据
+      window.localStorage.setItem('user', JSON.stringify(state.user))
     }
   },
   actions: {
