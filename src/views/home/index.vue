45a2260b8063dfa32e10362d6b88744312c0f1fb
jing <3223191529@qq.com>
2020-06-12 11:07:43 +0800

    首页顶栏布局处理

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="home-container">首页</div>
+  <div class="home-container">
+    <!-- 注意，可以支持插槽，slot就是插槽 -->
+    <van-nav-bar
+      class="app-nav-bar"
+    ><van-button
+    class="app-nav-btn"
+    type="warning"
+    icon="search"
+    round
+    slot="title">搜索</van-button>
+    </van-nav-bar>
+  </div>
 </template>
 <script>
 export default {
@@ -16,3 +27,22 @@ export default {
   methods: {}
 }
 </script>
+<style scoped lang="less">
+.home-container {
+  /deep/ .van-nav-bar_title {
+  max-width: none;
+}
+.app-nav-btn{
+  width: 277px;
+  height: 32px;
+  background: #5babfb;
+  border: none;
+  .van-icon {
+    font-size: 16px;
+  }
+  .van-button_text {
+    font-size: 14px;
+  }
+ }
+}
+</style>
