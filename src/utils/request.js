320292131f1eaf5a21c0159090cfa6c7fdfd2fd2
jing <3223191529@qq.com>
2020-06-12 10:06:28 +0800

    完成了token数据处理请求拦截器

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,21 @@
 import axios from 'axios'
+import store from '@/store/'
+// 在拦截器里面提供token
 const request = axios.create({
   baseURL: 'http://ttapi.research.itcast.cn/'
+})
+// 请求拦截器
+request.interceptors.request.use(function (config) {
+  // config参数就是请求的对象
+  // console.log(config)
+  const { user } = store.state
+  // 如果用户登录，才会有token数据
+  if (user) {
+    config.headers.Authorization = `Bearer ${user.token}`
+  }
+  return config
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error)
 })
 export default request
