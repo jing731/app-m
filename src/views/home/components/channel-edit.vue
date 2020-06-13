112db8c00c99b6c1740e5153cc69cbb5fec59f80
jing <3223191529@qq.com>
2020-06-14 01:28:27 +0800

    完成编辑的页面搭建

--- src/views/home/components/channel-edit.vue
+++ src/views/home/components/channel-edit.vue
@@ -1,5 +1,31 @@
 <template>
-  <div class="channel-edit">频道编辑</div>
+  <div class="channel-edit">
+  <!-- 头部部分 -->
+  <!-- :border='false'去除单元格的底边框 -->
+  <van-cell center :border='false'>
+    <div slot="title">我的频道</div>
+    <van-button
+    round type='danger'
+    plain size='mini'>编辑</van-button>
+  </van-cell>
+  <!-- 头部部分结束 -->
+  <!-- 宫格开始 -->
+  <van-grid :gutter="10">
+  <van-grid-item v-for="value in 8" :key="value" icon="photo-o"/>
+  </van-grid>
+   <!-- 宫格结束 -->
+    <!-- 头部部分 -->
+  <!-- :border='false'去除单元格的底边框 -->
+  <van-cell center :border='false'>
+    <div slot="title">我的频道</div>
+  </van-cell>
+  <!-- 头部部分结束 -->
+  <!-- 宫格开始 -->
+  <van-grid :gutter="10">
+  <van-grid-item v-for="value in 8" :key="value" icon="photo-o"/>
+  </van-grid>
+   <!-- 宫格结束 -->
+  </div>
 </template>
 <script>
 export default {
@@ -16,3 +42,8 @@ export default {
   methods: {}
 }
 </script>
+<style scoped lang="less">
+.channel-edit{
+  padding-top: 54px;
+}
+</style>
