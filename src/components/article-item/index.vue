08ca2e5872c4f6fc3a716ebb436909f71269cb9c
jing <3223191529@qq.com>
2020-06-13 18:44:01 +0800

    文章列表基本样式结构

--- src/components/article-item/index.vue
+++ src/components/article-item/index.vue
@@ -1,5 +1,32 @@
 <template>
-  <div class="article-item">文章列表++</div>
+  <div class="article-item">
+    <van-cell class="article-item">
+      <div slot="title">{{ article.title }}</div>
+      <div slot="label">
+        <div
+        v-if="article.cover.type === 3"
+        class="cover-wrap">
+          <div class="cover-warp-item">
+            <van-image
+            width="116"
+            height="73"
+            :v-for="(img,index) in article.cover.images"
+            :key="index"
+            :src="img" />
+          </div>
+        </div>
+        <div class="label-warp">
+          <span>{{ article.aut_name }}</span>
+          <span>{{ article.comm_count }}</span>
+          <span>{{ article.pubdate }}</span>
+        </div>
+      </div>
+      <van-image v-if="article.cover.type === 1"
+      width="116"
+      height="73"
+      :src="article.cover.images[0]" />
+    </van-cell>
+  </div>
 </template>
 <script>
 export default {
